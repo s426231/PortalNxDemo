de1761b0b05ea6035ec5debb44dde011123c804f
FRNIES <frederik.nies@btc-ag.com>
2021-03-24 11:21:31 +0100

    next js ts boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import React,{FC} from 'react';
+
+const Home: FC = () => {
+    return (
+        <div>
+            <h1>HALLO EIN SCHWEIN</h1>
+        </div>
+    )
+}
+export default Home;

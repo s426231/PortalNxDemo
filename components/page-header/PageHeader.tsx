8bb7e341400675335c4feb752c8d4efbbf8d6b43
papuslec <patryk.puslecki@btc-soft.pl>
2021-03-30 12:08:09 +0200

    test access

--- components/page-header/PageHeader.tsx
+++ components/page-header/PageHeader.tsx
@@ -1,14 +1,15 @@
-import React, {FC} from 'react';
-import {Typography} from "@material-ui/core";
+import React, { FC } from 'react';
+import { Typography } from "@material-ui/core";
 
 interface Props {
     headerText: string;
 }
 
-const PageHeader: FC<Props> = ({headerText}) => {
+const PageHeader: FC<Props> = ({ headerText }) => {
     return (
         <>
             <Typography variant={"h2"}>{headerText}</Typography>
+            <Typography variant={"h3"}>Test</Typography>
         </>
     )
 }

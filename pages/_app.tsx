64831e0f500d9a10c272f9187e5ac54a4fb38699
papuslec <patryk.puslecki@btc-soft.pl>
2021-03-30 15:14:39 +0200

    register component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import {AppProps} from 'next/app'
+import { AppProps } from 'next/app'
 import React from 'react'
 import Nav from "../components/navigation/Nav";
 import '../styles/globals.css'
-import {Grid} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
-function App({Component, pageProps}: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+
     return (
         <>
-            <Nav/>
-            <Grid container xs={12}>
+            <Nav />
+            <Grid container >
                 <Grid item xs={false} sm={2}>
                 </Grid>
                 <Grid container item xs={12} sm={8}>
